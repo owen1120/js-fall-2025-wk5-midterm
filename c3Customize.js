2d313285d5143c1df10929b95808a01cf1205ed0
owen1120 <110282480+owen1120@users.noreply.github.com>
2025-11-07 13:30:13 +0800

    fixed chart-pie

--- c3Customize.js
+++ c3Customize.js
@@ -1,23 +1,33 @@
 import c3 from 'c3';
 import 'c3/c3.css';
 
+import { getAllPackages } from './assets/js/state';
+
 export function initMyChart() {
+
+  const allData = getAllPackages();
+
+  const placeCounts = allData.reduce((acc, pkg) => {
+    const place = pkg.place;
+
+    acc[place] = (acc[place] || 0) + 1;
+    return acc;
+  }, {});
+
+  const chartDataColumns = Object.entries(placeCounts);
+
   c3.generate({
     bindto: '#chart',
 
     data: {
-      columns: [
-        ['台北', 65],
-        ['台中', 25],
-        ['高雄', 10]
-      ],
+      columns: chartDataColumns,
 
       type: 'donut',
       
       colors: {
         '台北': '#26C0C7',
         '台中': '#5151D3',
-        '高雄': '#E68618',
+        '高雄': '#E68618'
       }
     },
 
